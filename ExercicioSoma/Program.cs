76568bc559718a992c4e2164fe3150ebdedd4315
Fabiano Junior <99301175+fabianojrpro@users.noreply.github.com>
2023-01-15 19:18:05 -0300

    Add files via upload

---
+++ ExercicioSoma/Program.cs
@@ -0,0 +1,8 @@
+﻿// Pega os dois numeros digitados e soma 
+int primeiroNumero = int.Parse(Console.ReadLine());
+int segundoNumero = int.Parse(Console.ReadLine());
+
+int resultado = primeiroNumero + segundoNumero;
+
+Console.WriteLine("SOMA = " + resultado);
+
